0b1a08f59ddac27d9e17925bc5022e4fe613349a
Jeremy de Oliveira-Kumar <eremyj@gmail.com>
2021-02-28 08:22:27 +0800

    Updates to make it easier to use with html-minifier and webpack

---
+++ tools/browser.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+    Dictionary: Dictionary,
+    is_statement: is_statement,
+    List: List,
+    minify: minify,
+    parse: parse,
+    push_uniq: push_uniq,
+    TreeTransformer: TreeTransformer,
+    TreeWalker, TreeWalker
+}
+
+exports["Dictionary"] = Dictionary;
+exports["is_statement"] = is_statement;
+exports["List"] = List;
+exports["minify"] = minify;
+exports["parse"] = parse;
+exports["push_uniq"] = push_uniq;
+exports["TreeTransformer"] = TreeTransformer;
+exports["TreeWalker"] = TreeWalker;
