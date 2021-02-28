a98be020e56290a9d96b4a6542dde41ff68fca7e
Jeremy de Oliveira-Kumar <eremyj@gmail.com>
2021-02-28 09:32:07 +0800

    Changes to support browser behaviour

--- tools/browser.js
+++ tools/browser.js
@@ -1,20 +1,8 @@
 
+const minify = require('../lib/minify.js');
+
 module.exports = {
-    Dictionary: Dictionary,
-    is_statement: is_statement,
-    List: List,
-    minify: minify,
-    parse: parse,
-    push_uniq: push_uniq,
-    TreeTransformer: TreeTransformer,
-    TreeWalker, TreeWalker
+    minify: minify.minify
 }
 
-exports["Dictionary"] = Dictionary;
-exports["is_statement"] = is_statement;
-exports["List"] = List;
-exports["minify"] = minify;
-exports["parse"] = parse;
-exports["push_uniq"] = push_uniq;
-exports["TreeTransformer"] = TreeTransformer;
-exports["TreeWalker"] = TreeWalker;
+
